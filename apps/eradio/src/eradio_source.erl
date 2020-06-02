b263f29f157a44865da8778b9447e1f7902ea173
Jessa <git@jessa0.com>
2020-06-01 17:04:42 -0700

    implement vetoes, fan out source to streams through a stream manager

--- apps/eradio/src/eradio_source.erl
+++ apps/eradio/src/eradio_source.erl
@@ -7,7 +7,8 @@
 
 %% API
 -export([child_spec/0, start/0, stop/0, start_link/0,
-         player_state/0, play/0, pause/0, prev/0, next/0]).
+         player_state/0, play/0, pause/0, prev/0, next/0,
+         veto/1, vetoes/0]).
 -export_type([player_state/0]).
 
 %% gen_server callbacks
@@ -26,7 +27,9 @@
 
 -record(state,
         {source_port = undefined :: port(),
-         player_state = stopped :: player_state()}).
+         player_state = stopped :: player_state(),
+         current_track = undefined :: #track{} | unknown,
+         vetoes = #{} :: #{eradio_stream:listener_id() => true}}).
 
 %%
 %% API
@@ -65,6 +68,12 @@ prev() ->
 next() ->
     gen_server:call(?SERVER, {player_command, next}).
 
+veto(ListenerId) ->
+    gen_server:call(?SERVER, {veto, ListenerId}).
+
+vetoes() ->
+    gen_server:call(?SERVER, vetoes).
+
 %%
 %% gen_server callbacks
 %%
@@ -77,27 +86,17 @@ init([]) ->
 handle_call(player_state, _From, State) ->
     {reply, {ok, State#state.player_state}, State};
 
-handle_call({player_command, PlayerCommand}, From, #state{source_port = SourcePort} = State)
-  when is_port(SourcePort) ->
-    ?LOG_INFO("~1000p requesting player ~s", [From, PlayerCommand]),
-    PlayerCommandByte = case PlayerCommand of
-                            play  -> 1;
-                            pause -> 2;
-                            prev  -> 3;
-                            next  -> 4;
-                            _     -> unknown
-                        end,
-    Reply = case PlayerCommandByte of
-                unknown -> {error, bad_command};
-                _ ->
-                    port_command(State#state.source_port, <<PlayerCommandByte>>),
-                    ok
-            end,
-    {reply, Reply, State};
+handle_call({player_command, PlayerCommand}, From, State) ->
+    try handle_player_command(PlayerCommand, From, State)
+    catch _:Error -> {reply, {error, Error}, State} end;
 
-handle_call({player_command, PlayerCommand}, From, #state{source_port = undefined} = State) ->
-    ?LOG_INFO("~1000p requesting player ~s but player is dead!", [From, PlayerCommand]),
-    {reply, {error, source_dead}, State};
+handle_call({veto, ListenerId}, From, State) ->
+    try handle_veto(ListenerId, From, State)
+    catch _:Error -> {reply, {error, Error}, State} end;
+
+handle_call(vetoes, _From, State) ->
+    try count_vetoes(State) of Result -> {reply, {ok, Result}, State}
+    catch _:Error -> {reply, {error, Error}, State} end;
 
 handle_call(Request, From, State) ->
     ?LOG_WARNING("unknown call from ~1000p: ~1000p", [From, Request]),
@@ -168,6 +167,64 @@ spawn_source(State) ->
             State
     end.
 
+handle_player_command(PlayerCommand, From, #state{source_port = undefined} = State) ->
+    ?LOG_INFO("~1000p requesting player ~s but player is dead!", [From, PlayerCommand]),
+    {reply, {error, source_dead}, State};
+
+handle_player_command(PlayerCommand, From, State) ->
+    ?LOG_INFO("~1000p requesting player ~s", [From, PlayerCommand]),
+    Reply = send_player_command(PlayerCommand, State),
+    {reply, Reply, State}.
+
+send_player_command(_PlayerCommand, #state{source_port = undefined}) ->
+    {error, source_dead};
+send_player_command(PlayerCommand, State) ->
+    PlayerCommandByte = case PlayerCommand of
+                            play  -> 1;
+                            pause -> 2;
+                            prev  -> 3;
+                            next  -> 4;
+                            _     -> unknown
+                        end,
+    case PlayerCommandByte of
+        unknown -> {error, bad_command};
+        _ ->
+            port_command(State#state.source_port, <<PlayerCommandByte>>),
+            ok
+    end.
+
+handle_veto(_ListenerId, _From, #state{current_track = undefined}=State) ->
+    {reply, {error, stopped}, State};
+handle_veto(ListenerId, _From, #state{vetoes = Vetoes} = State) when erlang:is_map_key(ListenerId, Vetoes) ->
+    NewState = maybe_veto_skip(State),
+    {reply, ok, NewState};
+handle_veto(ListenerId, _From, State) ->
+    case State#state.current_track of
+        #track{id = TrackId} -> ?LOG_INFO("listener ~b vetoed ~b", [ListenerId, TrackId]);
+        unknown -> ?LOG_INFO("listener ~b vetoed <unknown>")
+    end,
+    NewState = maybe_veto_skip(State#state{vetoes = (State#state.vetoes)#{ListenerId => true}}),
+    {reply, ok, NewState}.
+
+maybe_veto_skip(#state{player_state = stopped}=State) ->
+    State;
+maybe_veto_skip(State) ->
+    {Listeners, Vetoes} = count_vetoes(State),
+    case map_size(Vetoes) >= length(Listeners) div 2 of
+        true  -> veto_skip(State);
+        false -> State
+    end.
+
+veto_skip(State) ->
+    send_player_command(next, State),
+    State.
+
+-spec count_vetoes(#state{}) -> {Listeners :: [eradio_stream:listener_id()], Vetoes :: #{eradio_stream:listener_id() => true}}.
+count_vetoes(State) ->
+    {ok, Listeners} = eradio_stream_manager:listener_ids(),
+    ActiveVetoes = maps:with(Listeners, State#state.vetoes),
+    {Listeners, ActiveVetoes}.
+
 handle_data(<<1, LevelByte, Message/binary>>, State) ->
     LevelAtom = case LevelByte of
                     1 -> error;
@@ -178,14 +235,26 @@ handle_data(<<1, LevelByte, Message/binary>>, State) ->
     ?LOG(LevelAtom, "~1000p ~s", [State#state.source_port, Message]),
     State;
 handle_data(<<2, Data/binary>>, State) ->
-    [eradio_stream:send_data(Stream, Data) || Stream <- eradio_stream:get_streams()],
+    eradio_stream_manager:send_data(Data),
     State;
 handle_data(<<3>>, State) ->
-    ?LOG_INFO("playback started: <unknown>"),
-    State#state{player_state = {playing, unknown}};
+    handle_new_player_state({playing, unknown}, State);
 handle_data(<<3, Id:128/integer, Name/binary>>, State) ->
-    ?LOG_INFO("playback started: ~s ~s", [base62:encode(Id, 22), Name]),
-    State#state{player_state = {playing, #track{id = Id, name = Name}}};
+    handle_new_player_state({playing, #track{id = Id, name = Name}}, State);
 handle_data(<<4>>, State) ->
+    handle_new_player_state(stopped, State).
+
+handle_new_player_state(PlayerState, #state{player_state = PlayerState}=State) ->
+    State;
+handle_new_player_state({playing, unknown}, State) ->
+    ?LOG_INFO("playback started: <unknown>"),
+    State#state{player_state = {playing, unknown}, current_track = unknown, vetoes = #{}};
+handle_new_player_state({playing, #track{id = TrackId}=Track}, #state{current_track = #track{id = TrackId}}=State) ->
+    ?LOG_INFO("playback resumed: ~s ~s", [base62:encode(Track#track.id, 22), Track#track.name]),
+    State#state{player_state = {playing, Track}};
+handle_new_player_state({playing, #track{}=Track}, State) ->
+    ?LOG_INFO("playback started: ~s ~s", [base62:encode(Track#track.id, 22), Track#track.name]),
+    State#state{player_state = {playing, Track}, current_track = Track, vetoes = #{}};
+handle_new_player_state(stopped, State) ->
     ?LOG_INFO("playback stopped"),
-    State#state{player_state = stopped}.
+    State#state{player_state = stopped, vetoes = #{}}.
